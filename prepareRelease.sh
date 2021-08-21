5d0e75d8064c42202936166f6297da36c000a125
ryan0583 <ryan0583@gmail.com>
2021-08-21 22:33:16 +0100

    Updating prepareRelease script

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -5,7 +5,7 @@ echo "Add the entries in the CHANGELOG folder to the CHANGELOG file..."
 
 echo "Commit the changelog changes..."
 git add .
-git commit -m "Update change log for release"
+git commit -m "Update change log for release $1"
 
 echo "Tag the last commit with the version number we're going to use for the release..."
 git tag $1

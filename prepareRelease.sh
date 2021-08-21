35f47e9101856333b0e85c52c87d3a2f2be5918b
ryan0583 <ryan0583@gmail.com>
2021-08-21 22:02:35 +0100

    Trying to fix

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Switch to dev branch and ensure we're up to date..."
-git checkout dev
-git pull
-git reset --hard origin/dev
-
 echo "Add the entries in the CHANGELOG folder to the CHANGELOG file..."
 ./writeChangelog.sh $1
 
@@ -14,9 +9,3 @@ git commit -m "Update change log"
 
 echo "Tag the last commit with the version number we're going to use for the release..."
 git tag $1
-
-echo "Checkout main and fastforward it to the tag we just created..."
-git checkout main
-git pull
-git reset --hard origin/main
-git merge --ff-only dev

fb779f08f7cf02be349337e6a7aa5a0830e34399
ryan0583 <ryan0583@gmail.com>
2021-08-21 21:45:58 +0100

    Scripting messing

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+git branch
+
+git fetch
+git checkout main
+git branch -D dev
+
 echo "Switch to dev branch and ensure we're up to date..."
 git checkout dev
 git pull
@@ -13,3 +19,9 @@ git commit -m "Update change log"
 
 echo "Tag the last commit with the version number we're going to use for the release..."
 git tag $1
+
+echo "Checkout main and fastforward it to the tag we just created..."
+git branch -D main
+git checkout main
+git pull
+git merge --ff-only dev

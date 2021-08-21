2328c7725511cfd683e85710679bd10037e79751
ryan0583 <ryan0583@gmail.com>
2021-08-21 20:53:21 +0100

    Fix script again

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-git fetch
-git checkout main
-git branch -D dev
-
 echo "Switch to dev branch and ensure we're up to date..."
 git checkout dev
 git pull
@@ -17,9 +13,3 @@ git commit -m "Update change log"
 
 echo "Tag the last commit with the version number we're going to use for the release..."
 git tag $1
-
-echo "Checkout main and fastforward it to the tag we just created..."
-git branch -D main
-git checkout main
-git pull
-git merge --ff-only dev

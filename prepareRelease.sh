e9fa5f24620cfa122cd41c1bde6106ad790ea87b
ryan0583 <ryan0583@gmail.com>
2021-08-21 20:16:06 +0100

    Adding logging to prepareRelease file

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Switch to dev branch and ensure we're up to date
+echo "Switch to dev branch and ensure we're up to date..."
 git checkout dev
 git pull
 
-# Add the entries in the CHANGELOG folder to the CHANGELOG file
+echo "Add the entries in the CHANGELOG folder to the CHANGELOG file..."
 ./writeChangelog.sh $1
 
-# Commit the changelog changes
+echo "Commit the changelog changes..."
 git add .
 git commit -m "Update change log"
 
-# Tag the last commit with the version number we're going to use for the release
+echo "Tag the last commit with the version number we're going to use for the release..."
 git tag $1
 
-# Checkout main and fastforward it to the tag we just created.
+echo "Checkout main and fastforward it to the tag we just created..."
 git checkout main
 git pull
 git merge --ff-only $1

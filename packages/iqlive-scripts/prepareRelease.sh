92ebbf17eb2760c6ddfd6f10c1c2b1cc29e8f9d8
ryan0583 <ryan0583@gmail.com>
2021-08-24 15:44:49 +0100

    Changed release script location

--- packages/iqlive-scripts/prepareRelease.sh
+++ packages/iqlive-scripts/prepareRelease.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Add the entries in the CHANGELOG folder to the CHANGELOG file..."
-./scripts/writeChangelog.sh $1
+./packages/iqlive-scripts/writeChangelog.sh $1
 
 echo "Commit the changelog changes..."
 git add .
-git commit -m "Update change log for release $1"
+git commit -m "[changelog] Update change log for release $1"
 
 echo "Tag the last commit with the version number we're going to use for the release..."
 git tag $1

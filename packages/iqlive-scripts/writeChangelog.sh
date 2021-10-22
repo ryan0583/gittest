2a13ac28fb22d5e3e47b7b43238fadd7d997f70a
ryan0583 <ryan0583@gmail.com>
2021-10-22 17:28:09 +0100

    Updating writeChanelog script so it in line with the one in our repo

--- packages/iqlive-scripts/writeChangelog.sh
+++ packages/iqlive-scripts/writeChangelog.sh
@@ -5,23 +5,30 @@ function appendChangelogLine {
 }
 
 function appendChangelogSection {
-    appendChangelogLine ""
-
     DIR="./CHANGELOG/$1"
+
+    WROTELINE=0
 
     if [ "$(ls -A "$DIR")" ]; then
         for file in "$DIR"/*
             do 
                 if [[ "$file" != *"README.md"* ]];then
+                    if [[ $WROTELINE == 0 ]]; then
+                        appendChangelogLine ""
+                    fi
+
                     echo "Processing $file..."
                     appendChangelogLine "- $(cat $file)"
                     rm "$file"
+                    WROTELINE=1
                 fi
         done
     fi
 
-    appendChangelogLine ""
-    appendChangelogLine "### $1"
+    if [[ $WROTELINE == 1 ]]; then
+        appendChangelogLine ""
+        appendChangelogLine "### $1"
+    fi
 }
 
 appendChangelogSection "Removed"
